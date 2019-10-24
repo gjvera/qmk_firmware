729a616d4f1399e2adcd7367f20525589eece64e
gjvera <git@gabrieljosephvera.com>
2019-10-24 12:33:02 -0700

    Adding yabai keys and rebasing

--- keyboards/keebio/nyquist/keymaps/gjvera/keymap.c
+++ keyboards/keebio/nyquist/keymaps/gjvera/keymap.c
@@ -29,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LCTL,  KC_LGUI, KC_LALT, KC_LGUI, KC_SPC,   MO(2),  MO(3),  KC_ENT,   M(4), M(9), M(10),   M(5) 
           ),
     LAYOUT(
-            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
-            M(3), KC_Q, KC_W, KC_E, KC_R, KC_T, M(1), KC_EQL, KC_7, KC_8, KC_9, KC_BSPC, 
-            M(6), KC_QUOTE, KC_S, KC_D, TD(LPAREN_BRACE), TD(RPAREN_BRACE), KC_BSLS, KC_MINS, KC_4, KC_5, KC_6, KC_TRNS, 
+	   KC_F1,   KC_F2,   KC_F3,   LCA(KC_Z),   KC_F5,   KC_F6,   KC_F7,   KC_F8,   LCA(KC_C),   KC_F10,  KC_F11,  KC_F12, 
+	   M(3), KC_Q, KC_W, KC_E, LALT(KC_R), KC_T, M(1), KC_EQL, KC_7, KC_8, KC_9, KC_BSPC, 
+            LGUI(KC_X), KC_QUOTE, KC_S, KC_D, TD(LPAREN_BRACE), TD(RPAREN_BRACE), KC_BSLS, KC_MINS, KC_4, KC_5, KC_6, KC_TRNS, 
             KC_LSFT, KC_GRV, KC_X, KC_C, KC_V, KC_B, KC_N, KC_SLSH, KC_1, KC_2, KC_3, KC_ENT, 
             KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, KC_NO, KC_SPC, KC_SPC, KC_NO, KC_LEFT, KC_0, KC_UP, KC_RGHT 
           ),
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     LAYOUT(
             KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
-            KC_F1, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN1, KC_WH_D, KC_F6, KC_F7, KC_VOLD, KC_MUTE, KC_VOLU, KC_F11, KC_F12, 
-            KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_WH_U, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_DEL, 
+            KC_F1, KC_MS_BTN2, LALT(KC_K), KC_MS_BTN1, KC_WH_D, KC_F6, KC_F7, KC_VOLD, KC_MUTE, KC_VOLU, KC_F11, KC_F12, 
+            KC_TRNS, LALT(KC_H), LALT(KC_J), LALT(KC_L), KC_WH_U, KC_TRNS, KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_DEL, 
             KC_TRNS, RGB_TOG, RGB_MOD, KC_VOLD, KC_MUTE, KC_VOLU, RGB_HUD, RGB_SAI, RGB_SAD, KC_HOME, KC_END, KC_TRNS, 
             RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY 
           ),
